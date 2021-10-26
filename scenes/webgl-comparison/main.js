cf246baf3c343936ca0872fb7b3276c1e1f3d595
liangrongjia <liangrongjia999@foxmail.com>
2021-10-27 01:30:17 +0800

    refactor(sences/webgl-comparison): 重构整理 sences/webgl-comparison/main.js

--- scenes/webgl-comparison/main.js
+++ scenes/webgl-comparison/main.js
@@ -2,7 +2,6 @@ import * as _THREE from '../../lib/three.module.js'
 import { GLTFLoader } from './lib/GLTFLoader.module.js'
 import { OrbitControls } from './lib/OrbitControls.module.js'
 import { Stats } from './lib/stats.module.js'
-import dat from './lib/dat.gui.module.js'
 import { RGBELoader } from './lib/RGBELoader.module.js'
 import * as RayTracingRenderer from '../../src/main.js'
 import { EquirectangularToCubeGenerator } from './lib/EquirectangularToCubeGenerator.module.js'
@@ -16,39 +15,74 @@ const THREE = {
   ...RayTracingRenderer
 }
 
-let renderer
-let controls
-let scene
-
-const camera = new THREE.PerspectiveCamera()
-camera.position.set(64, 32, 16)
-camera.fov = 65
-
-const stats = new Stats()
-stats.setMode(0) // 0: fps, 1: ms
-stats.domElement.style.position = 'absolute'
-stats.domElement.style.left = '0px'
-stats.domElement.style.top = '0px'
-document.body.appendChild(stats.domElement)
-
 init()
 
-window.addEventListener('resize', resize)
-
 async function init() {
-  const [envMap, envMapLDR, gltf] = await Promise.all([
-    load(THREE.RGBELoader, '../envmaps/street-by-water.hdr'),
-    load(THREE.TextureLoader, 'envmap.jpg'),
-    load(THREE.GLTFLoader, 'scene.gltf'),
-  ])
+  const camera = createCamera()
+  const stats = createStats()
+  const state = {
+    renderer: null,
+    controls: null,
+    scene: null,
+    setRenderer: (r) => state.renderer = r,
+    setControls: (c) => state.controls = c,
+    setScene: (s) => state.scene = s,
+  }
 
-  scene = new THREE.Scene()
+  await initGui({ state, camera })
+  mountStats(stats)
+  addListeners({ state, camera })
 
+  const tick = getTickFunc({ state, camera, stats })
+
+  tick(performance.now())
+}
+
+function getTickFunc({ state, camera, stats }) {
+  const tick = (now = 0) => {
+    state.controls.update()
+
+    camera.focus = state.controls.target.distanceTo(camera.position)
+
+    stats.begin()
+    if (state.renderer.sync) state.renderer.sync(now)
+    state.renderer.render(state.scene, camera)
+    stats.end()
+
+    requestAnimationFrame(tick)
+  }
+  return tick
+}
+
+function mountStats(stats) {
+  document.body.appendChild(stats.domElement)
+  document.querySelector('#loading').remove()
+}
+
+function addListeners({ state, camera }) {
+  window.addEventListener('resize', () => resize(state.renderer, camera))
+}
+
+function createCamera() {
+  const camera = new THREE.PerspectiveCamera()
+  camera.position.set(64, 32, 16)
+  camera.fov = 65
+  return camera
+}
+
+function createStats() {
+  const stats = Stats()
+  stats.setMode(0) // 0: fps, 1: ms
+  stats.domElement.style.position = 'absolute'
+  stats.domElement.style.left = '0px'
+  stats.domElement.style.top = '0px'
+  return stats
+}
+
+function createModel(gltf) {
   const model = gltf.scene
-
   model.scale.set(0.5, 0.5, 0.5)
   model.rotateY(Math.PI / 2)
-
   model.traverse(child => {
     if (child instanceof THREE.Mesh) {
       // only necessary for WebGLRenderer
@@ -59,68 +93,62 @@ async function init() {
       child.material.transparent = true
     }
   })
-
-  const uiCallbacks = {
-    WebGL: () => initWebGL(envMapLDR, model),
-    RayTracing: () => initRayTracing(envMap, model)
-  }
-
-  const gui = new dat.GUI()
-  gui.add(uiCallbacks, 'WebGL')
-  gui.add(uiCallbacks, 'RayTracing')
-
-  uiCallbacks.RayTracing()
-
-  resize()
-
-  // THREE.DefaultLoadingManager.onLoad = tick;
-  tick()
-
-  document.querySelector('#loading').remove()
+  return model
 }
 
-function resize() {
-  if (renderer.domElement.parentElement) {
-    const width = renderer.domElement.parentElement.clientWidth
-    const height = renderer.domElement.parentElement.clientHeight
-    renderer.setSize(width, height)
+async function initGui({ state, camera }) {
+  const [envMap, envMapLDR, gltf] = await Promise.all([
+    load(RGBELoader, '../envmaps/street-by-water.hdr'),
+    load(THREE.TextureLoader, './envmap.jpg'),
+    load(GLTFLoader, './scene.gltf'),
+  ])
 
-    camera.aspect = width / height
-    camera.updateProjectionMatrix()
+  const model = createModel(gltf)
+
+  const toRenderMode = {
+    WebGL: () => {
+      unloadRenderer(state.renderer, state.controls)
+      const { scene, renderer, controls } = initWebGL({ envMapLDR, model, camera })
+      state.setRenderer(renderer)
+      state.setControls(controls)
+      state.setScene(scene)
+      resize(renderer, camera)
+    },
+    RayTracing: () => {
+      unloadRenderer(state.renderer, state.controls)
+      const { scene, renderer, controls } = initRayTracing({ envMap, model, camera })
+      state.setRenderer(renderer)
+      state.setControls(controls)
+      state.setScene(scene)
+      resize(renderer, camera)
+    }
   }
-}
 
-function tick(time) {
-  controls.update()
-  camera.focus = controls.target.distanceTo(camera.position)
-  stats.begin()
-
-  if (renderer.sync) {
-    renderer.sync(time)
-  }
-
-  renderer.render(scene, camera)
-  stats.end()
-
-  requestAnimationFrame(tick)
-}
-
-function initWebGL(envMapLDR, model) {
-  unloadRenderer(renderer)
-  renderer = new THREE.WebGLRenderer({
-    antialias: true
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'r') toRenderMode.RayTracing()
+    else if (e.key === 'w') toRenderMode.WebGL()
   })
-  initRenderer(renderer)
 
+  toRenderMode.RayTracing()
+}
+
+function resize(renderer, camera) {
+  if (!renderer.domElement.parentElement) return
+  const width = renderer.domElement.parentElement.clientWidth
+  const height = renderer.domElement.parentElement.clientHeight
+  renderer.setSize(width, height)
+
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+}
+
+function initWebGL({ envMapLDR, model, camera }) {
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  initRenderer(renderer)
   renderer.shadowMap.enabled = true
   renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-  scene = new THREE.Scene()
-  scene.add(model)
-
   const dirLight = new THREE.DirectionalLight(0xff3300, 0.3)
-  // dirLight.target.position = controls.target
-  scene.add(dirLight.target)
   dirLight.target.position.set(0, 20, 0)
   dirLight.castShadow = true
   dirLight.position.setFromSphericalCoords(100, -1.31, 4.08)
@@ -130,47 +158,51 @@ function initWebGL(envMapLDR, model) {
   dirLight.shadow.camera.right = 50
   dirLight.shadow.camera.top = 50
   dirLight.shadow.camera.bottom = -50
-  scene.add(dirLight)
 
   const ambLight = new THREE.AmbientLight(0xffffff, 0.2)
-  scene.add(ambLight)
-
-  // const helper = new THREE.CameraHelper(dirLight.shadow.camera);
-  // scene.add(helper);
-
-  const equiToCube = new THREE.EquirectangularToCubeGenerator(envMapLDR)
+  const equiToCube = new EquirectangularToCubeGenerator(envMapLDR)
   const cubeMap = equiToCube.renderTarget
   const cubeMapTexture = equiToCube.update(renderer)
 
+  const scene = new THREE.Scene()
+  scene.background = cubeMap
+  scene.add(model)
+  scene.add(dirLight.target)
+  scene.add(dirLight)
+  scene.add(ambLight)
   scene.traverse(child => {
-    if (child.material) {
-      child.material.envMap = cubeMapTexture
-    }
+    if (child.material) child.material.envMap = cubeMapTexture
   })
 
-  scene.background = cubeMap
+  const controls = createControls({ renderer, camera })
+
+  return { scene, renderer, controls }
 }
 
-function initRayTracing(envMap, model) {
-  unloadRenderer(renderer)
-  renderer = new THREE.RayTracingRenderer()
+function initRayTracing({ envMap, model, camera }) {
+  const envLight = new RayTracingRenderer.EnvironmentLight(envMap)
+
+  const scene = new THREE.Scene()
+  scene.add(model)
+  scene.add(envLight)
+
+  const renderer = new RayTracingRenderer.RayTracingRenderer()
   initRenderer(renderer)
 
-  scene = new THREE.Scene()
+  const controls = createControls({ renderer, camera })
 
-  scene.add(model)
+  return { scene, renderer, controls }
+}
 
-  const envLight = new THREE.EnvironmentLight(envMap)
-  scene.add(envLight)
+function createControls({ renderer, camera }) {
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.screenSpacePanning = true
+  controls.target.set(0, 20, 0)
+  return controls
 }
 
 function initRenderer(renderer) {
   document.body.appendChild(renderer.domElement)
-  resize()
-
-  controls = new THREE.OrbitControls(camera, renderer.domElement)
-  controls.screenSpacePanning = true
-  controls.target.set(0, 20, 0)
 
   renderer.gammaOutput = true
   renderer.gammaFactor = 2.2
@@ -181,7 +213,7 @@ function initRenderer(renderer) {
   renderer.bounces = 3
 }
 
-function unloadRenderer(renderer) {
+function unloadRenderer(renderer, controls) {
   if (renderer) {
     renderer.dispose()
     renderer.domElement.remove()
@@ -193,7 +225,7 @@ function unloadRenderer(renderer) {
 
 function load(loader, url) {
   return new Promise(resolve => {
-    const l = new loader()
-    l.load(url, resolve, undefined, exception => { throw exception })
+    const ldr = new loader()
+    ldr.load(url, resolve, undefined, exception => { throw exception })
   })
 }
